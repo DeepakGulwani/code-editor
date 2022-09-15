5b4c44b58862fa4f9bb009fff7db3b7071bdb887
DeepakGulwani <105498098+DeepakGulwani@users.noreply.github.com>
2022-09-15 16:07:57 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://github.com/DeepakGulwani">Deepak</a>
                 </h4>
             </footer>
         </div>
